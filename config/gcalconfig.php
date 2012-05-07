47184b6aa5659dcefac3730b38e0369315177272
Joe Auty <joe@netmusician.org>
2012-05-07 03:42:26 -0400

    update default config

--- config/gcalconfig.php
+++ config/gcalconfig.php
@@ -1,13 +1,13 @@
 <?php
 // Your application client ID, provided post app registry in your Google APIs Console 
-$config['client_id'] = '320783750865.apps.googleusercontent.com';
+$config['client_id'] = "";
 
 // Your application client secret, provided post app registry in your Google APIs Console 
-$config['client_secret'] = 'rIr8_NDOqcz2LdaFW0KmQ8pj';
+$config['client_secret'] = "";
 
 // Your application's name (will be displayed on the Google authentication/authorization page)
 $config['application_name'] = "My Awesome Application";
 
 // Your application's API key (can be used for fetching public data without oAuth2)
-$config['api_key'] = "AIzaSyCj4N0O-iF-0ooCda0pqEG7RsAlsXkE-Bs";
+$config['api_key'] = "";
 ?>
